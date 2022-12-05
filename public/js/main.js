b0c4055dea0c88cd3d5fb3dfd818f660e85b7e8c
abhi <abhishekpandit1212@gmail.com>
2022-12-05 11:00:09 +0530

    weather file updated - show temp value properly on frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,22 @@
 const cityName = document.getElementById("cityName")
 const submitBtn = document.getElementById("submitBtn")
 const city_name = document.getElementById("city_name")
-const temp = document.getElementById("temp")
+const temp_real_value = document.getElementById("temp_real_value")
 const temp_status = document.getElementById("temp_status")
+const data_hide = document.querySelector('.middle_layer')
+const daytoday = document.getElementById("daytoday")
+const today_date = document.getElementById("today_date")
+const month = document.getElementById("month")
+
+
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let d = new Date();
+let day = d.getDay();
+let cmonth = d.getMonth()
+month.innerHTML = months[cmonth]
+today_date.innerHTML = d.getDate() 
+daytoday.innerHTML = days[day];
 
 const getInfo = async(e) => {
     e.preventDefault()
@@ -11,6 +25,7 @@ const getInfo = async(e) => {
 
     if(cityVal === ""){
         city_name.innerText = "Plz write the name before search"
+        data_hide.classList.add("data_hide")
     }else{
         try{
             let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=689d581be4ad2ff0ee02725de752f55b`
@@ -19,11 +34,26 @@ const getInfo = async(e) => {
             const arrData = [data]
 
             city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`
-            temp.innerText = `${arrData[0].main.temp}`
-            temp_status.innerText = `${arrData[0].weather[0].main}`
+            temp_real_value.innerText = arrData[0].main.temp
+            const tempMood = arrData[0].weather[0].main
+
+            
+
+            if(tempMood == "Clear"){
+                temp_status.innerHTML = '<i class="fa-solid fa-sun" style="color: #eccc68"></i>'
+            }
+            else if(tempMood == "Clouds"){
+                temp_status.innerHTML = '<i class="fa-solid fa-cloud" style="color: #f1f2f6"></i>'
+            }else if(tempMood == "Rain"){
+                temp_status.innerHTML = '<i class="fa-solid fa-cloud-rain style="color: #a4b0be" "></i>'
+            }else{
+                temp_status.innerHTML = '<i class="fa-solid fa-cloud" style="color: #eccc68"></i>'
+            }
+            data_hide.classList.remove("data_hide")
         }
         catch{
             city_name.innerText = "Plz enter the city name properly"
+            data_hide.classList.add("data_hide")
         }
     }
 }
